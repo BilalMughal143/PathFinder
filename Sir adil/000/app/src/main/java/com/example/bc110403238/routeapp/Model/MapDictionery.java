9fdbb55445c7f5aa738802a4351644a72e913f56
BilalMughal143 <bilal.muhghal@gmail.com>
2015-07-08 11:28:17 +0500

    initial commit

---
+++ Sir adil/000/app/src/main/java/com/example/bc110403238/routeapp/Model/MapDictionery.java
@@ -0,0 +1,17 @@
+package com.example.bc110403238.routeapp.Model;
+
+/**
+ * Created by zaib on 6/13/2015.
+ */
+public class MapDictionery {
+
+    String my_currentAddrss = null;
+
+    public String getMy_currentAddrss() {
+        return my_currentAddrss;
+    }
+
+    public void setMy_currentAddrss(String my_currentAddrss) {
+        this.my_currentAddrss = my_currentAddrss;
+    }
+}
---
+++ Sir adil/000/app/src/main/java/com/example/bc110403238/routeapp/Pathparse.java
@@ -0,0 +1,19 @@
+package com.example.bc110403238.routeapp;
+
+import android.graphics.Path;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by bc110403238 on 06-Jun-15.
+ */
+public class Pathparse {
+
+    public void myFunction(String data) {
+
+
+    }
+
+
+}
